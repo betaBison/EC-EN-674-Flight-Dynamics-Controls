5af4d384f1875edf5291d4b4d22fa25202098442
betaBison <knowles.derek@gmail.com>
2019-02-07 22:04:52 -0700

    added rotation vehicle to body

--- tools/tools.py
+++ tools/tools.py
@@ -19,3 +19,9 @@ def Quaternion2Euler(e):
     theta = asin(2*(e0*e2-e1*e3))
     psi = atan2(2*(e0*e3+e1*e2),(e0**2+e1**2-e2**2-e3**2))
     return phi, theta, psi
+
+def RotationVehicle2Body(phi,theta,psi):
+    result = np.array([[cos(theta)*cos(psi),cos(theta)*sin(psi),-sin(theta)],
+                       [sin(phi)*sin(theta)*cos(psi)-cos(phi)*sin(psi),sin(phi)*sin(theta)*sin(psi)+cos(phi)*cos(psi),sin(phi)*cos(theta)],
+                       [cos(phi)*sin(theta)*cos(psi)+sin(phi)*sin(psi),cos(phi)*sin(theta)*sin(psi)-sin(phi)*cos(psi),cos(phi)*cos(theta)]])
+    return result
