461485c557c3d84fc55ab03356ece87bdad6e291
betaBison <knowles.derek@gmail.com>
2019-02-11 15:21:12 -0700

    Rotation Body to Vehicle

--- tools/tools.py
+++ tools/tools.py
@@ -25,3 +25,9 @@ def RotationVehicle2Body(phi,theta,psi):
                        [sin(phi)*sin(theta)*cos(psi)-cos(phi)*sin(psi),sin(phi)*sin(theta)*sin(psi)+cos(phi)*cos(psi),sin(phi)*cos(theta)],
                        [cos(phi)*sin(theta)*cos(psi)+sin(phi)*sin(psi),cos(phi)*sin(theta)*sin(psi)-sin(phi)*cos(psi),cos(phi)*cos(theta)]])
     return result
+
+def RotationBody2Vehicle(phi,theta,psi):
+    result = np.array([[cos(theta)*cos(psi),cos(theta)*sin(psi),-sin(theta)],
+                       [sin(phi)*sin(theta)*cos(psi)-cos(phi)*sin(psi),sin(phi)*sin(theta)*sin(psi)+cos(phi)*cos(psi),sin(phi)*cos(theta)],
+                       [cos(phi)*sin(theta)*cos(psi)+sin(phi)*sin(psi),cos(phi)*sin(theta)*sin(psi)-sin(phi)*cos(psi),cos(phi)*cos(theta)]])
+    return result.T
