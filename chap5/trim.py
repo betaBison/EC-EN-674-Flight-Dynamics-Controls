e2429112531f1a5b88359e1ba3f062911dfb631f
betaBison <knowles.derek@gmail.com>
2019-02-13 21:30:45 -0700

    fixed bugs, optimize does NOTHING

--- chap5/trim.py
+++ chap5/trim.py
@@ -30,7 +30,7 @@ def compute_trim(mav, Va, gamma):
     delta_a = 0.005
     delta_e = -0.2
     delta_r = 0.008
-    delta_t = 0.5
+    delta_t = 0.4
     delta0 = np.array([[delta_a, delta_e, delta_r, delta_t]]).T
     x0 = np.concatenate((state0, delta0), axis=0)
     # define equality constraints
@@ -64,7 +64,7 @@ def compute_trim(mav, Va, gamma):
     trim_input = np.array([res.x[13:17]]).T
     return trim_state, trim_input
 
-def calcX_dot_star(Va_star,gamma_star):
+def calcX_dot_star(Va,gamma):
     gamma_star = gamma
     Va_star = Va
     x_dot_star = np.array([[0.0],  # pn_dot don't care
@@ -83,9 +83,11 @@ def calcX_dot_star(Va_star,gamma_star):
 
 # objective function to be minimized
 def trim_objective(x, mav, Va, gamma):
+    #print("x=",x)
+    #print("Va=",Va)
+    #print("Gamma=",gamma)
     Va_star = Va
     gamma_star = gamma
-    #
     phi_star = 0.0
     theta = 0.0
     alpha_star = theta-gamma
@@ -93,7 +95,7 @@ def trim_objective(x, mav, Va, gamma):
     psi = 0.0
     beta_star = chi-psi
 
-    x_dot_star = calcX_dot_star(Va,gamma,R_star)
+    x_dot_star = calcX_dot_star(Va_star,gamma_star)
     u_star = Va_star*np.cos(alpha_star)*np.cos(beta_star)
     v_star = Va_star*np.sin(beta_star)
     w_star = Va_star*np.sin(alpha_star)*np.cos(beta_star)
@@ -105,30 +107,31 @@ def trim_objective(x, mav, Va, gamma):
     delta_e_star = (((MAV.Jxz*(p_star**2-r_star**2)+(MAV.Jx-MAV.Jz)*p_star*r_star)/(0.5*MAV.rho*Va_star**2*MAV.c*MAV.S_wing))-MAV.C_m_0-MAV.C_m_alpha*alpha_star-MAV.C_m_q*MAV.c*q_star/(2.*Va_star))
 
     # F.2
-    Cx = -dyn.CD(alpha_star)*np.cos(alpha_star) + dyn.CL(alpha_star)*np.sin(alpha_star)
+    Cx = -CD(alpha_star)*np.cos(alpha_star) + CL(alpha_star)*np.sin(alpha_star)
     Cxq = -MAV.C_D_q*np.cos(alpha_star) + MAV.C_L_q*np.sin(alpha_star)
     Cxde = -MAV.C_D_delta_e*np.cos(alpha_star) + MAV.C_L_delta_e*np.sin(alpha_star)
 
-    delta_t_star = np.sqrt((2.*MAV.mass*(-r_star*v_star+q_star*w_star+MAV.gravity*np.sin(theta_star))
-        -MAV.rho*Va_star**2*MAV.S_wing*(Cx+Cxq*MAV.c*q_star/(2.*Va_star)+Cxde*delta_e_star))
+    delta_t_star = np.sqrt((2.*MAV.mass*(-r_star*v_star+q_star*w_star+MAV.gravity*np.sin(theta_star)) \
+        -MAV.rho*Va_star**2*MAV.S_wing*(Cx+Cxq*MAV.c*q_star/(2.*Va_star)+Cxde*delta_e_star)) \
         /(MAV.rho*MAV.S_prop*MAV.C_prop*MAV.k_motor**2)+Va_star**2/MAV.k_motor**2)
 
     # F.3
-    A = np.matrix([[MAV.C_p_delta_a,MAV.C_p_delta_r],
+    A = np.array([[MAV.C_p_delta_a,MAV.C_p_delta_r],
                   [MAV.C_r_delta_a,MAV.C_r_delta_r]])
-    B = np.matrix([[(-MAV.gamma1*p_star*q_star+MAV.gamma2*q_star*r_star)/(0.5*MAV.rho*Va_star**2*MAV.S_wing*MAV.b)
+    B = np.array([[(-MAV.gamma1*p_star*q_star+MAV.gamma2*q_star*r_star)/(0.5*MAV.rho*Va_star**2*MAV.S_wing*MAV.b)
                     -MAV.C_p_0-MAV.C_p_beta*beta_star-MAV.C_p_p*MAV.b*p_star/(2.*Va_star)-MAV.C_p_r*MAV.b*r_star/(2.*Va_star)],
                    [(-MAV.gamma7*p_star*q_star+MAV.gamma1*q_star*r_star)/(0.5*MAV.rho*Va_star**2*MAV.S_wing*MAV.b)
                     -MAV.C_r_0-MAV.C_r_beta*beta_star-MAV.C_r_p*MAV.b*p_star/(2.*Va_star)-MAV.C_r_r*MAV.b*r_star/(2.*Va_star)]])
-    delta_result = np.cross(np.linalg.inv(A),B)
+    delta_result = np.matmul(np.linalg.inv(A),B)
     delta_a_star = delta_result.item(0)
-    delta_r_star = relta_result.item(1)
+    delta_r_star = delta_result.item(1)
 
     delta = np.array([[delta_a_star, delta_e_star, delta_r_star, delta_t_star]]).T
 
     f_result = calcLinearDerivatives(x_dot_star,delta,alpha_star,Va_star,beta_star)
 
     J = np.linalg.norm(x_dot_star - f_result)**2
+    print(J)
     return J
 
 def calcLinearDerivatives(x_star,u_star,alpha,Va,beta):
@@ -164,17 +167,17 @@ def calcLinearDerivatives(x_star,u_star,alpha,Va,beta):
 
 
     # position dynamics
-    Cx = -CD(alpha)*cos(alpha) + CL(alpha)*sin(alpha)
-    Cxq = -MAV.C_D_q*cos(alpha) + MAV.C_L_q*sin(alpha)
-    Cxde = -MAV.C_D_delta_e*cos(alpha) + MAV.C_L_delta_e*sin(alpha)
-    Cz = -CD(alpha)*sin(alpha) - CL(alpha)*cos(alpha)
-    Czq = -MAV.C_D_q*sin(alpha) - MAV.C_L_q*cos(alpha)
-    Czde = -MAV.C_D_delta_e*sin(alpha) - MAV.C_L_delta_e*cos(alpha)
+    Cx = -CD(alpha)*np.cos(alpha) + CL(alpha)*np.sin(alpha)
+    Cxq = -MAV.C_D_q*np.cos(alpha) + MAV.C_L_q*np.sin(alpha)
+    Cxde = -MAV.C_D_delta_e*np.cos(alpha) + MAV.C_L_delta_e*np.sin(alpha)
+    Cz = -CD(alpha)*np.sin(alpha) - CL(alpha)*np.cos(alpha)
+    Czq = -MAV.C_D_q*np.sin(alpha) - MAV.C_L_q*np.cos(alpha)
+    Czde = -MAV.C_D_delta_e*np.sin(alpha) - MAV.C_L_delta_e*np.cos(alpha)
 
-    u_dot = r*v-q*w + MAV.gravity*np.sin(theta) + MAV.rho*Va**2*MAV.S_wing*(Cx+Cxq*MAV.c*q/(2.*Va)+Cxde*delta_e)/(2.*MAV.mass) +
+    u_dot = r*v-q*w + MAV.gravity*np.sin(theta) + MAV.rho*Va**2*MAV.S_wing*(Cx+Cxq*MAV.c*q/(2.*Va)+Cxde*delta_e)/(2.*MAV.mass) + \
         MAV.rho*MAV.S_prop*MAV.C_prop*((MAV.k_motor*delta_t)**2-Va*2)/(2.*MAV.mass)
-    v_dot = p*w-r*u + MAV.gravity*np.cos(theta)*np.sin(phi)+
-        MAV.rho*Va**2*MAV.S_wing*(MAV.C_Y_0+MAV.C_Y_beta*beta*MAV.C_Y_p*MAV.b*p/(2.*Va)+MAV.C_Y_r*MAV.b*r/(2.*Va)+MAV.C_Y_delta_a*delta_a+MAV.C_Y_delta_r*delta_r))/(2.*MAV.mass)
+    v_dot = p*w-r*u + MAV.gravity*np.cos(theta)*np.sin(phi)+ \
+        MAV.rho*Va**2*MAV.S_wing*(MAV.C_Y_0+MAV.C_Y_beta*beta*MAV.C_Y_p*MAV.b*p/(2.*Va)+MAV.C_Y_r*MAV.b*r/(2.*Va)+MAV.C_Y_delta_a*delta_a+MAV.C_Y_delta_r*delta_r)/(2.*MAV.mass)
     w_dot = q*u-p*v + MAV.gravity*np.cos(theta)*np.cos(phi)+MAV.rho*Va**2*MAV.S_wing*(Cz+Czq*MAV.c*q/(2.*Va)+Czde*delta_e)/(2.*MAV.mass)
 
     # rotational kinematics
@@ -183,10 +186,10 @@ def calcLinearDerivatives(x_star,u_star,alpha,Va,beta):
     psi_dot = q*np.sin(phi)/np.cos(theta)+r*np.cos(phi)/np.cos(theta)
 
     # rotatonal dynamics
-    p_dot = MAV.gamma1*p*q - MAV.gamma2*q*r +
+    p_dot = MAV.gamma1*p*q - MAV.gamma2*q*r + \
         0.5*MAV.rho*Va**2*MAV.S_wing*MAV.b*(MAV.C_p_0+MAV.C_p_beta*beta*MAV.C_p_p*MAV.b*p/(2.*Va)+MAV.C_p_r*MAV.b*r/(2.*Va)+MAV.C_p_delta_a*delta_a+MAV.C_p_delta_r*delta_r)
     q_dot = MAV.gamma5*p*r - MAV.gamma6*(p**2-r**2) + MAV.rho*Va**2*MAV.S_wing*MAV.c*(MAV.C_m_0+MAV.C_m_alpha*alpha+MAV.C_m_q*MAV.c*q/(2.*Va)+MAV.C_m_delta_e*delta_e)/(2.*MAV.Jy)
-    r_dot = MAV.gamma7*p*q - MAV.gamma1*q*r +
+    r_dot = MAV.gamma7*p*q - MAV.gamma1*q*r + \
         0.5*MAV.rho*Va**2*MAV.S_wing*MAV.b*(MAV.C_r_0+MAV.C_r_beta*beta*MAV.C_r_p*MAV.b*p/(2.*Va)+MAV.C_r_r*MAV.b*r/(2.*Va)+MAV.C_r_delta_a*delta_a+MAV.C_r_delta_r*delta_r)
 
     # collect the derivative of the states
@@ -198,7 +201,7 @@ def CD(alpha):
     '''
     UAV book equation 4.11
     '''
-    result = MAV.C_D_p + ((MAV.C_L_0+alpha*MAV.C_L_alpha)**2)/(pi*MAV.e*MAV.AR)
+    result = MAV.C_D_p + ((MAV.C_L_0+alpha*MAV.C_L_alpha)**2)/(np.pi*MAV.e*MAV.AR)
     #result = (1-calcSigma(alpha))*(MAV.C_D_0 + MAV.C_D_alpha*alpha)+calcSigma(alpha)*(2.0*np.sign(alpha)*sin(alpha)**2*cos(alpha))
     return result
 
@@ -207,7 +210,7 @@ def CL(alpha):
     This is a linear coefficient model that is not valid over a wide
     range of angles of attack. UAV Book equation 4.13
     '''
-    result = (1-calcSigma(alpha))*(MAV.C_L_0 + MAV.C_L_alpha*alpha)+calcSigma(alpha)*(2.0*np.sign(alpha)*sin(alpha)**2*cos(alpha))
+    result = (1-calcSigma(alpha))*(MAV.C_L_0 + MAV.C_L_alpha*alpha)+calcSigma(alpha)*(2.0*np.sign(alpha)*np.sin(alpha)**2*np.cos(alpha))
     return result
 
 def calcSigma(alpha):
