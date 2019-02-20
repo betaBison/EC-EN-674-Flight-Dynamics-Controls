51d7a8819c5f6a075ac7855e3a86209dcfc0424e
betaBison <knowles.derek@gmail.com>
2019-02-20 10:32:12 -0700

    chap 6 & 7 templates

---
+++ parameters/control_parameters.py
@@ -0,0 +1,38 @@
+import sys
+sys.path.append('..')
+import numpy as np
+import chap5.transfer_function_coef as TF
+
+gravity =
+sigma =
+Va0 =
+
+#----------roll loop-------------
+roll_kp =
+roll_kd =
+
+#----------course loop-------------
+course_kp =
+course_ki =
+
+#----------sideslip loop-------------
+sideslip_ki =
+sideslip_kp =
+
+#----------yaw damper-------------
+yaw_damper_tau_r =
+yaw_damper_kp =
+
+#----------pitch loop-------------
+pitch_kp =
+pitch_kd =
+K_theta_DC =
+
+#----------altitude loop-------------
+altitude_kp =
+altitude_ki =
+altitude_zone =
+
+#---------airspeed hold using throttle---------------
+airspeed_throttle_kp =
+airspeed_throttle_ki =
